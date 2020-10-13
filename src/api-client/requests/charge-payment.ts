4555516e1ae26886f2ce91a585cc7e1b40c86421
mike <jfkz@users.noreply.github.com>
2020-10-13 12:35:10 +0300

    feat: add confirm and charge methods

---
+++ src/api-client/requests/charge-payment.ts
@@ -0,0 +1,80 @@
+import { HttpRequestMethod } from '../../http-client/http-client';
+import { Schema, SchemaPropertyType as PropType } from '../../serialization/schema';
+import { ApiClient } from '../api-client';
+import { ResponsePayload as BaseResponsePayload } from '../response-payload';
+
+//=========//
+// REQUEST //
+//=========//
+
+/** @see https://oplata.tinkoff.ru/develop/api/payments/charge-request/ */
+export interface ChargePaymentRequestPayload {
+  /** Идентификатор платежа в системе банка	*/
+  PaymentId: number;
+  /** Идентификатор автоплатежа	*/
+  RebillId: number;
+  /** Получение покупателем уведомлений на электронную почту */
+  SendEmail?: boolean;
+  /** Электронная почта покупателя */
+  InfoEmail?: string;
+  /** IP-адрес покупателя	*/
+  IP?: string;
+  /** https://oplata.tinkoff.ru/develop/api/request-sign/ */
+  Token?: string;
+}
+
+const ChargePaymentRequestSchema: Schema = [];
+
+//==========//
+// RESPONSE //
+//==========//
+
+/** https://oplata.tinkoff.ru/develop/api/autopayments/charge-response/ */
+export interface ChargePaymentResponsePayload extends BaseResponsePayload {
+  /** Сумма в копейках */
+  Amount: number;
+  /** Идентификатор заказа в системе продавца	*/
+  OrderId: string;
+  /** Уникальный идентификатор транзакции в системе банка	*/
+  PaymentId: number;
+}
+
+const ChargePaymentResponseSchema: Schema = [
+  {
+    property: 'Amount',
+    type: PropType.MoneyFromPenny,
+  },
+];
+
+
+//==========//
+// FUNCTION //
+//==========//
+
+export async function chargePayment(options: {
+  apiClient: ApiClient;
+  payload: ChargePaymentRequestPayload;
+
+}): Promise<ChargePaymentResponsePayload> {
+
+  const { apiClient } = options;
+
+  const {...restPayload } = options.payload;
+
+  const $payload: any = {
+    ...restPayload,
+  };
+
+  const response = await apiClient.sendRequest<ChargePaymentResponsePayload>({
+    request: {
+      url: 'Charge',
+      method: HttpRequestMethod.POST,
+      payload: $payload,
+    },
+    requestSchema: ChargePaymentRequestSchema,
+    responseSchema: ChargePaymentResponseSchema,
+  });
+
+  return response.payload;
+
+}
---
+++ src/api-client/requests/confirm-payment.ts
@@ -0,0 +1,85 @@
+import { HttpRequestMethod } from '../../http-client/http-client';
+import { Schema, SchemaPropertyType as PropType } from '../../serialization/schema';
+import { ApiClient } from '../api-client';
+import { ResponsePayload as BaseResponsePayload } from '../response-payload';
+import { validateAndPrepareReceipt } from './common/receipt';
+import { Receipt } from './common/receipt';
+
+
+//=========//
+// REQUEST //
+//=========//
+
+/** @see https://oplata.tinkoff.ru/develop/api/payments/confirm-request/ */
+export interface ConfirmPaymentRequestPayload {
+  /** Идентификатор платежа в системе банка	*/
+  PaymentId: number;
+  /** Сумма в копейках */
+  Amount?: number;
+  /** IP-адрес покупателя	*/
+  IP?: string;
+  /** https://oplata.tinkoff.ru/develop/api/request-sign/ */
+  Token?: string;
+  /** Массив данных чека. См. https://oplata.tinkoff.ru/develop/api/payments/init-request/#Receipt */
+  Receipt?: Receipt;
+}
+
+const ConfirmPaymentRequestSchema: Schema = [];
+
+//==========//
+// RESPONSE //
+//==========//
+
+/** https://oplata.tinkoff.ru/develop/api/autopayments/confirm-response/ */
+export interface ConfirmPaymentResponsePayload extends BaseResponsePayload {
+  /** Идентификатор заказа в системе продавца	*/
+  OrderId: string;
+  /** Сумма в копейках */
+  Amount: number;
+  /** Уникальный идентификатор транзакции в системе банка	*/
+  PaymentId: number;
+}
+
+const ConfirmPaymentResponseSchema: Schema = [
+  {
+    property: 'Amount',
+    type: PropType.MoneyFromPenny,
+  },
+];
+
+
+//==========//
+// FUNCTION //
+//==========//
+
+export async function confirmPayment(options: {
+  apiClient: ApiClient;
+  payload: ConfirmPaymentRequestPayload;
+
+}): Promise<ConfirmPaymentResponsePayload> {
+
+  const { apiClient } = options;
+
+  const { Receipt, ...restPayload } = options.payload;
+
+  const $payload: any = {
+    ...restPayload,
+  };
+
+  if (Receipt) {
+    $payload.Receipt = validateAndPrepareReceipt(Receipt);
+  }
+
+  const response = await apiClient.sendRequest<ConfirmPaymentResponsePayload>({
+    request: {
+      url: 'Confirm',
+      method: HttpRequestMethod.POST,
+      payload: $payload,
+    },
+    requestSchema: ConfirmPaymentRequestSchema,
+    responseSchema: ConfirmPaymentResponseSchema,
+  });
+
+  return response.payload;
+
+}
